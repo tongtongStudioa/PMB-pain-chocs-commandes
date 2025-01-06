582eae0f661ceb71253de7f115d722eb5e9c368b
tongtongStudioa <>
2025-01-06 11:56:48 +0100

    2025 second semester update

--- main.py
+++ main.py
@@ -18,35 +18,35 @@ from affiche_infos_utiles import generer_rapport
 
 listes_classes = [
         {'nom':"FISE_BAT3_1",
-         'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=4795&projectId=2&calType=ical&lastDate=2042-08-14',
+         'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9789&projectId=2&calType=ical&lastDate=2042-08-14',
          'effectif':25
          },
          {'nom':"FISE_BAT3_2",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=5726&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9794&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':25
           },
          {'nom':"FISA_BAT3",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=5728&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9822&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':35
           },
          {'nom':"EIT3",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=5727&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9800&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':35
           },
          {'nom':"FISA_BAT4",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=2640&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9779&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':32,
           },
          {'nom':"FISE_BAT4_1",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=2308&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9540&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':20,
           },
          {'nom':"FISE_BAT4_2",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=2314&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9543&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':23,
           },
          {'nom':"EIT4",
-          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=2761&projectId=2&calType=ical&lastDate=2042-08-14',
+          'url_edt':'https://ade-usmb-ro.grenet.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=b5cfb898a9c27be94975c12c6eb30e9233bdfae22c1b52e2cd88eb944acf5364c69e3e5921f4a6ebe36e93ea9658a08f,1&resources=9519&projectId=2&calType=ical&lastDate=2042-08-14',
           'effectif':30
           },
 ]
@@ -59,5 +59,5 @@ horaires_pauses = [{"heure": 9, "minutes": 30},
 
 
 #Génère un rapport pdf et sur la console
-generer_rapport(horaires_pauses,listes_classes,"Presence 1 semestre PMB(1)")
+generer_rapport(horaires_pauses,listes_classes,"Presences PMB Semestre 2 2024-2025")
 
